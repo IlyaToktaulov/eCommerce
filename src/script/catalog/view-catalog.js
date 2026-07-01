c10d2eae82e47c8820a7a191add126e545834a5e
IlyaToktaulov <toktaulovia@mail.ru>
2026-07-01 19:13:01 +0300

    Now you can view the product information by clicking on it.

--- src/script/catalog/view-catalog.js
+++ src/script/catalog/view-catalog.js
@@ -1,6 +1,7 @@
 export class View_Catalog {
     constructor({itemToBag}) {
         this.catalogNode = document.querySelector('.js-catalog-output');
+        this.itemCardNode = document.querySelector('.js-popup-output');
         this.itemToBag = itemToBag;
     }
 
@@ -12,7 +13,7 @@ export class View_Catalog {
 
     renderItem = (item) => {
         const div = document.createElement('div');
-        const model = document.createElement('a');
+        const model = document.createElement('p');
         const series = document.createElement('p');
         const img = document.createElement('img');
         const price = document.createElement('p');
@@ -20,9 +21,19 @@ export class View_Catalog {
 
         div.setAttribute('class', 'item-card');
         div.dataset.id = item.id; 
+        div.onclick = () => {
+            this.renderPopupItem({
+                model: item.model,
+                series: item.series,
+                img: item.img,
+                price: item.price,
+                description: item.description
+            });
+
+            this.itemCardNode.classList.toggle('popup-open');
+        }
 
         model.setAttribute('class', 'item-model');
-        model.setAttribute('href', '');
         model.innerText = item.model;
 
         series.setAttribute('class', 'item-series');
@@ -56,5 +67,39 @@ export class View_Catalog {
             this.catalogNode.append(div);
         }
 
+    }
+
+    renderPopupItem = (item) => {
+        const div = document.createElement('div');
+        const model = document.createElement('p');
+        const series = document.createElement('p');
+        const img = document.createElement('img');
+        const price = document.createElement('p');
+        const description = document.createElement('p');
+        const closeBtn = document.createElement('btn');
+
+
+        div.setAttribute('class', 'item-card-content');
+
+        model.setAttribute('class', 'item-card-model');
+        model.innerText = item.model;
+
+        series.setAttribute('class', 'item-card-series');
+        series.innerText = item.series;
+
+        img.setAttribute('class', 'item-card-img');
+        img.setAttribute('src', item.imageURL);
+
+        price.setAttribute('class', 'item-card-price');
+        price.innerText = `${item.price} Руб`;
+
+        closeBtn.innerText = '✖';
+        closeBtn.onclick = () => {
+            this.itemCardNode.classList.toggle('popup-open');
+            div.remove();
+        }
+
+        div.append(img, model, series, price, closeBtn);
+        this.itemCardNode.append(div);
     }
 }
